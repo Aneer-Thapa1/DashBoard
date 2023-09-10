1018c4dd5acbfddda3d2dd83c27f0940a5f6c9fb
Anir Jung Thapa <anir234thapa@gmail.com>
2023-09-10 17:59:23 +0545

    Final

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    productName: "JavaScript Tutorial",
+    productNumber: "85473",
+    paymentStatus: "Due",
+    status: "Pending",
+  },
+  {
+    productName: "CSS Full Course",
+    productNumber: "98752",
+    paymentStatus: "Refunded",
+    status: "Declined",
+  },
+  {
+    productName: "Flex-box Tutorial",
+    productNumber: "36452",
+    paymentStatus: "Paid",
+    status: "Active",
+  },
+];
